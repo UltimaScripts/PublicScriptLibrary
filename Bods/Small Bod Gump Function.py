90b0edeb788dc715c716731ff9353e1bbb20abc1
Ultima Online <131027345+UltimaScripts@users.noreply.github.com>
2023-09-22 15:05:29 +0000

    Update Small Bod Gump Function.py

--- Bods/Small Bod Gump Function.py
+++ Bods/Small Bod Gump Function.py
@@ -18,45 +18,45 @@ def smallbodinfo(debug = False):
         inital_gump_list.append(gump)
     for x in inital_gump_list[1]:
         if x.Pages != None:
-                for pagecount in range(len(x.Pages)):
-                    ele = x.Pages[pagecount].GumpElements
-                    for entry in range(len(ele)):
-                        textentry = str(ele[entry].Text)
-                        if textentry != "" or textentry != None:
-                            if "a bulk order" in str(textentry).lower():
+            for pagecount in range(len(x.Pages)):
+                ele = x.Pages[pagecount].GumpElements
+                for entry in range(len(ele)):
+                    textentry = str(ele[entry].Text)
+                    if textentry != "" or textentry != None:
+                        if "a bulk order" in str(textentry).lower():
+                            if debug == True:
+                                print str("Gump ID (not serial): " + str(x.ID))
+                            #smallbod_return_list.append(int(x.ID))
+                        if "amount to make:" in str(textentry).lower():
+                            #if it finds "amount to make:"
+                            #it advances the entry by 1
+                            num1 = entry + 1
+                            #the amount text on servuo was +1 entry
+                            #if it's not on your shard, open the debug window
+                            #(top right corner bug icon), gump tab
+                            #find "amount to make:" in the bod gump
+                            #for example if the text number is + 3 spots
+                            #you would use num1 = entry + 3
+                            if debug == True:
+                                print str(ele[num1].Text)
+                            smallbod_return_list.append(int(ele[num1].Text))
+                        if "item requested:" in str(textentry).lower():
+                            #same applies for "item requested:"
+                            #as explained for "amount to make:"
+                            num2 = entry + 2
+                            if debug == True:
+                                print str(ele[num2].Text)
+                            smallbod_return_list.append(str(ele[num2].Text))
+                        if "must be exceptional" in str(textentry).lower():
+                            if debug == True:
+                                print("exceptional")
+                            smallbod_return_list.append(str("exceptional"))
+                        for material in material_list:
+                            if material in str(textentry).lower():
                                 if debug == True:
-                                    print str("Gump ID (not serial): " + str(x.ID))
-                                #smallbod_return_list.append(int(x.ID))
-                            if "amount to make:" in str(textentry).lower():
-                                #if it finds "amount to make:"
-                                #it advances the entry by 1
-                                num1 = entry + 1
-                                #the amount text on servuo was +1 entry
-                                #if it's not on your shard, open the debug window
-                                #(top right corner bug icon), gump tab
-                                #find "amount to make:" in the bod gump
-                                #for example if the text number is + 3 spots
-                                #you would use num1 = entry + 3
-                                if debug == True:
-                                    print str(ele[num1].Text)
-                                smallbod_return_list.append(int(ele[num1].Text))
-                            if "item requested:" in str(textentry).lower():
-                                #same applies for "item requested:"
-                                #as explained for "amount to make:"
-                                num2 = entry + 2
-                                if debug == True:
-                                    print str(ele[num2].Text)
-                                smallbod_return_list.append(str(ele[num2].Text))
-                            if "must be exceptional" in str(textentry).lower():
-                                if debug == True:
-                                    print("exceptional")
-                                smallbod_return_list.append(str("exceptional"))
-                            for material in material_list:
-                                if material in str(textentry).lower():
-                                    if debug == True:
-                                        print(material)
-                                    smallbod_return_list.append(str(material))
-                                    break
+                                    print(material)
+                                smallbod_return_list.append(str(material))
+                                break
     return smallbod_return_list
 
 # Set debug to True, to print what it's finding.
