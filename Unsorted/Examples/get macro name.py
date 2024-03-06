684e749ed620ee963016d8783bd9ad266de8108a
Ultima Online <131027345+UltimaScripts@users.noreply.github.com>
2024-03-06 22:42:58 +0000

    Add files via upload

---
+++ Unsorted/Examples/get macro name.py
@@ -0,0 +1,7 @@
+from ClassicAssist.Data.Macros import MacroManager
+
+#prints the macro name.
+print(MacroManager.GetInstance().GetCurrentMacro())
+
+#This is used for something else...
+#print(MacroManager.GetInstance().CurrentMacro)
