da962955f57c2f9ac4b35fb07a7c621b8fd88272
Ultima Online <131027345+UltimaScripts@users.noreply.github.com>
2025-11-21 14:40:27 +0000

    Update goto pathfind def.py

--- Travel/goto Rail System/goto pathfind def.py
+++ Travel/goto Rail System/goto pathfind def.py
@@ -1,4 +1,6 @@
-def goto(xx, yy, zz):
+def goto(xx, yy, zz, tolerance=0):
     Pathfind(xx, yy, zz)
     while Pathfinding():
         Pause(100)
+        if Distance(xx, yy) <= tolerance:
+            Pathfind(-1)
